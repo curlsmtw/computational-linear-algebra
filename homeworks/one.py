cb01c661c957801e018820c2110441202b306830
Abbie Mathew <abbie.r.mathew@gmail.com>
2025-01-26 23:26:34 -0500

    feat: Added Comments with the assigned problems.

---
+++ homeworks/one.py
@@ -0,0 +1,24 @@
+'''
+1. Write Python code to create a (4 x 8) matrix of integers from 1 to 32. 
+Extract the element corresponding to your birth month. Extract the element
+corresponding to your birth date. Print the output.
+
+2. Write Python code to create a (10 x 10) matrix C of integers from 1 to
+100.
+(a) Extract the submatrix C1 comprising the first five rows and five
+columns.
+(b) Cut original matrix in 4 blocks such that: C1 is submatrix comprising
+the first five rows and five columns; C2 submatrix comprising the first five
+rows and last five columns; C3 is submatrix comprising the last five rows
+and first five columns. C4 is submatrix comprising the last five rows and
+five columns.
+Rearrange the blocks to get a newM atrix =
+
+| C4 C3 |
+| C2 C1 |
+
+3. Create two (4 x 4) matrices: a matrix of all ones and a diagonal matrix
+where diagonal elements are 1, 4, 9, and 16. Perform premultiplying and
+posmultiplying of the matrix of ones by the diagonal matrix. Make a
+conclusion about the result.
+'''
