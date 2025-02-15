afa512e7ecef9eeb1957e58b599d272999b253a7
Abbie Mathew <abbie.r.mathew@gmail.com>
2025-02-15 13:31:57 -0500

    feat: Finishing up the rest of the problems

--- homeworks/two.py
+++ homeworks/two.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sympy as smp
 import scipy.linalg as lg
+
+
 # Problem 1: Write a Python program to find the Reduced Row Echelon Form (RREF)
 # of a given matrix A =1 1 4
 #                   −0.5 1 2
@@ -33,7 +35,6 @@ print("sympy 2A:\n", sym_matrix_twoa)
 rref_matrix_twoa = sym_matrix_twoa.rref()[0]
 print("rref(2A):\n",rref_matrix_twoa)
 
-
 matrix_twob = np.array([[2,2,-1,2], [1,2,1,4], [-1,-1,2,1]])
 print("Original 2B Matrix:", matrix_twob)
 
@@ -42,6 +43,7 @@ print("sympy 2b:\n", sym_matrix_twob)
 
 rref_matrix_twob = sym_matrix_twob.rref()[0]
 print("rref(2B):\n",rref_matrix_twob)
+
 
 # Problem 3: Use RREF to solve the linear system A~x = ~b.
 # A = 2 2 −1
@@ -52,8 +54,6 @@ print("rref(2B):\n",rref_matrix_twob)
 #     4
 #     1
 
-
-
 matrix_three_a = smp.Matrix([[2,2,-1], [1,2,1], [-1,-1,2]])
 matrix_three_b = smp.Matrix([[2], [4], [1]])
 
@@ -62,9 +62,7 @@ print("3A Augmented Matrix:\n", aug_matrix)
 rref_matrix_three = aug_matrix.rref()[0]
 print("RREF of the augmented matrix:", rref_matrix_three)
 solution = smp.linsolve((matrix_three_a, matrix_three_b))
-print("Soulution:", solution)
-
-
+print("Solution:", solution)
 
 # Problem 4: 4. Use an LU -factorization of the coefficient matrix to solve the linear system
 # A~x = ~b.
@@ -74,6 +72,7 @@ print("Soulution:", solution)
 # ~b = 2
 #      4
 #      1
+
 matrix_four_a = np.array([[2,2,-1], [1,2,1], [-1,-1,2]])
 matrix_four_b = np.array([[2], [4], [1]])
 
@@ -82,6 +81,8 @@ print("L=\n", four_l, "\nU=\n", four_u, "\nP=\n", four_p)
 four_y = lg.solve(four_l, matrix_four_b)
 four_x = lg.solve(four_u, four_y)
 print("x=\n", four_x)
+
+
 # Problem 5: 5. Use an LU -factorization of the coefficient matrix to solve the linear system
 # A~x = ~b.
 # A = 4 −1 −1
@@ -100,6 +101,7 @@ five_y = lg.solve(five_l, matrix_five_b)
 five_x = lg.solve(five_u, five_y)
 print("x=\n", five_x)
 
+
 # Problem 6: One application of LU is computing the determinant. 
 # We know that the determinant of a triangular matrix is the product 
 # of the diagonals. We also know that the determinant of the product
@@ -110,3 +112,18 @@ print("x=\n", five_x)
 # of L are all ones, then the determinant of A is simply the product of 
 # the diagonals of U multiplied by the determinant of permutation  matrix.
 # Try it in Python and compare the result to the result of np.linalg.det(A)
+
+def compute_determinant(matrix):
+    P, L, U = lg.lu(matrix)
+    det_A_LU = np.prod(np.diag(U)) * np.linalg.det(P)
+    det_A_numpy = np.linalg.det(matrix)
+    return det_A_LU, det_A_numpy
+
+if __name__ == "__main__":
+    matrix_six = np.array([[4,-1,-1], [-1,4,-1], [-1,-1,4]])
+    det_A_LU, det_A_numpy = compute_determinant(matrix_six)
+    
+    print("Original Matrix:")
+    print(matrix_six)
+    print("Determinant using LU decomposition:", det_A_LU)
+    print("Determinant using numpy's built-in function:", det_A_numpy)
