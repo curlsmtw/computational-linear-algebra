63f0daba8ba09fca6abf0a0a2d3611d591dd4795
Abbie Mathew <abbie.r.mathew@gmail.com>
2025-02-18 21:03:00 -0500

    feat: Set up instructions for next set of problems

---
+++ homeworks/three.py
@@ -0,0 +1,12 @@
+# Consider a linear transformation T : R^2 → R^2 that will 
+# translate points # by (3, -2) and then rotate 45 degrees 
+# about the origin. Come up with a matrix of transformation. 
+# Choose a prototype (can be any but let it have not less
+# than four points) and plot it together with its image.
+
+
+# 2. Consider a linear transformation T : R^2 → R^2 that will 
+# rotate points 60 degrees # about the point (6,8). Come up  with 
+# a matrix of transformation. Choose a prototype (can beany but 
+# let it have not less than four points) and plot it together with 
+#its image.
