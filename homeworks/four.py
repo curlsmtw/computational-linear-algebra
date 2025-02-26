257a446e56f78e3bdd634347c0eb46aea3f1ae63
Abbie Mathew <abbie.r.mathew@gmail.com>
2025-02-26 10:23:22 -0500

    feat: Adding instructions for homework four

---
+++ homeworks/four.py
@@ -0,0 +1,30 @@
+# 1. Consider the following vectors in R^3:
+# u1 = [ 1
+#       3
+#      −2]
+# u2 = [5
+#       1
+#       4 ]
+#  y = [1
+#       4
+#       5]
+
+# (a) Find the best approximation of y in the subspace W = Span {u1, u2},
+# which is the projection of y onto the subspace spanned by u1 and
+# u2.
+
+
+# (b) Visualize the subspace spanned by the orthogonalized vectors and the
+# projection of y onto this subspace in a 3D plot. Include the following
+# in your plot:
+# • The vectors u1 and u2,
+# • The vector y,
+# • The projection of y onto the subspace spanned by u1 and u2,
+# • The orthogonal complement of y relative to the subspace.
+
+
+# 2. For the 4 × 3 matrix A = [ 3 −5 1,
+#                               1 1 1,
+#                               −1 5 −2,
+#                               3 −7 8],
+# find an orthonormal basis for Col(A).
